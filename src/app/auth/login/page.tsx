ca384f16f6f8ef033543441d407614cfd2c1fb0b
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2024-07-14 17:40:42 +0600

    login

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,105 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+
+const page = () => {
+  return (
+    <div className="flex flex-wrap justify-center items-center h-screen p-1">
+      <Card
+        className="w-full sm:w-1/2 lg:w-1/2 m-2 backgr bg-[url('/imgs/back-img.png')] bg-contain bg-no-repeat bg-center relative"
+        style={{ width: "550px", minHeight: "660px" }}
+      >
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+          <Image
+            className="mx-auto"
+            src="/imgs/logo.png"
+            width="120"
+            height="120"
+            alt="Flash Bot Logo"
+          />
+          <h1 className="text-3xl font-bold">FlashBot</h1>
+        </div>
+      </Card>
+      <div className="w-full sm:w-1/2 lg:w-1/2 h-screen">
+        <div className="flex items-center justify-center text-center text-3xl font-bold mb-14">
+          <div className="mr-1">
+            <Image
+              className="mx-auto"
+              src="/imgs/logo.png"
+              width="80"
+              height="80"
+              alt="Flash Bot Logo"
+            />
+          </div>
+          <div>FlashBot</div>
+        </div>
+        <Card className="mx-auto p-4" style={{ width: "500px" }}>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Welcome! to Flashbot</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email*</Label>
+                <Input id="email" placeholder="Enter your email" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password*</Label>
+                <Input
+                  id="password"
+                  placeholder="Enter your password"
+                  type="password"
+                />
+                <span className="text-sm text-muted-foreground">
+                  Must be at least 8 characters.
+                </span>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-2">
+            <Button className="w-full block mb-3">Login</Button>
+            <Button className="w-full mb-8" variant="outline">
+              <FcGoogle
+                className="inline mr-2"
+                style={{ width: "25px", height: "50px" }}
+              />
+              Login with Google
+            </Button>
+
+            <span className="text-sm">
+              Don&apos;t have an account?{" "}
+              <Link href={"/"} style={{ color: "#2DD2CE" }}>
+                Sign UP
+              </Link>
+            </span>
+          </CardFooter>
+        </Card>
+
+        <div className="w-full flex flex-col mt-12">
+          <div className="flex justify-between  w-full">
+            <p className="text-sm lg:mx-8">© 2024 Zorpvideo</p>
+            <div className="flex items-end justify-end text-sm space-x-1 text-blue-800">
+              <Link href={"/"}>Term & Condition</Link>
+              <span className="text-bold text-white">|</span>
+              <Link href={"/"}>Privacy & Policy</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
