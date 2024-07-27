e95a42865f64d7b6f545494f3398aa8e9e656690
fahimhossen <fahim.hossen@koneksys.com>
2024-07-27 18:18:12 +0600

    solved the issue

--- src/components/executors/executors-table.tsx
+++ src/components/executors/executors-table.tsx
@@ -117,33 +117,32 @@ export function ExecutorTable<Executor, TValue>({
             className="w-[250px] md:max-w-sm mb-2 mr-6"
           />
 
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"ghost"}
-                className="border-2"
-                onClick={() => setShow(true)}
-              >
-                <LuSettings2 className="mr-2" />
-                Filter
-              </Button>
-            </PopoverTrigger>
-            {show && (
-              <div
-                ref={btnRef}
-                className="absolute z-50 w-80 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 2xl:top-[180px] sm:top-[130px]"
-              >
-                <FilterOption />
+          <div ref={btnRef}>
+            <Popover>
+              <PopoverTrigger asChild>
                 <Button
-                  variant={"secondary"}
-                  className="w-full dark:text-[#FE0FE2] dark:bg-[#700162]"
-                  onClick={() => setShow(false)}
+                  variant={"ghost"}
+                  className="border-2"
+                  onClick={() => setShow(!show)}
                 >
-                  Apply
+                  <LuSettings2 className="mr-2" />
+                  Filter
                 </Button>
-              </div>
-            )}
-          </Popover>
+              </PopoverTrigger>
+              {show && (
+                <div className="absolute z-50 w-80 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 2xl:top-[180px] sm:top-[130px] right-[350px]">
+                  <FilterOption />
+                  <Button
+                    variant={"secondary"}
+                    className="w-full dark:text-[#FE0FE2] dark:bg-[#700162]"
+                    onClick={() => setShow(false)}
+                  >
+                    Apply
+                  </Button>
+                </div>
+              )}
+            </Popover>
+          </div>
 
           <div>
             <Button variant={"secondary"} className="ml-4">
