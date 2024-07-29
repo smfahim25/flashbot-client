4c8110d6dfd3fb7e8e95a0fe7f27c7202acc08cc
fahimhossen <fahim.hossen@koneksys.com>
2024-07-29 17:58:24 +0600

    update toggle button for notificatiohn page

--- src/app/(dashboard)/dashboard/settings/notification/page.tsx
+++ src/app/(dashboard)/dashboard/settings/notification/page.tsx
@@ -8,7 +8,14 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Select, SelectContent, SelectItem, SelectSeparator, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Toggle } from "@/components/ui/toggle";
 import { useState } from "react";
@@ -23,161 +30,178 @@ const lexend = Lexend({
   subsets: ["vietnamese"],
 });
 export default function Page() {
-    const [support, setSupport] = useState(false);
-    const handleToggleChange = () => {
-        setSupport(!support);
-      };
+  const [proNoti, setProNoti] = useState(false);
+  const [receiveMsg, setReceiveMsg] = useState(false);
+  const [getPush, setGetPush] = useState(false);
+  const [receiveCupon, setReceiveCupon] = useState(false);
+  const [receiveAccount, setReceiveAccount] = useState(false);
+  const handleProNoti = () => {
+    setProNoti(!proNoti);
+  };
+  const handleReceiveMsg = () => {
+    setReceiveMsg(!receiveMsg);
+  };
+  const handleGetPush = () => {
+    setGetPush(!getPush);
+  };
+  const handleReceiveCupon = () => {
+    setReceiveCupon(!receiveCupon);
+  };
+  const handleReceiveAccount = () => {
+    setReceiveAccount(!receiveAccount);
+  };
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4  p-4 pt-6 md:p-8 mb-16">
         <div className="flex justify-between items-center">
           <div>
             <h6
-              className={`font-semibold text-[24px] leading-10 ${lexend.className}`}
+              className={`font-semibold text-[24px] leading-10 text-center ${lexend.className}`}
             >
               Notification Settings
             </h6>
             <Breadcrumbs items={breadcrumbItems} />
           </div>
         </div>
-         <Card className="w-full border-none px-5 rounded-xl p-5 mt-5 ">
-            <div className="grid grid-cols-3 w-1/2 my-4 ">
+        <Card className="w-full border-none px-5 rounded-xl p-5 mt-5 ">
+          <div className="grid grid-cols-3 w-1/2 my-4 ">
             <Link href="/dashboard/settings">
-            <h4 className="font-normal text-sm text-center p-2 text-[#737373] border-b border-b-[#D9DFEB] cursor-pointer hover:border-b-[#FE0FE2]">
+              <h4 className="font-normal text-sm text-center p-2 text-[#737373] border-b border-b-[#D9DFEB] cursor-pointer hover:border-b-[#FE0FE2]">
                 General Settings
-            </h4>
-
+              </h4>
             </Link>
 
             <Link href="/dashboard/settings/password">
-            <h4 className="font-[400] text-sm text-inner text-[#737373] ext-center p-2 border-b border-b-[#D9DFEB] cursor-pointer hover:border-b-[#FE0FE2]">
-            Password Settings
-            </h4>
+              <h4 className="font-[400] text-sm text-inner text-[#737373] text-center p-2 border-b border-b-[#D9DFEB] cursor-pointer hover:border-b-[#FE0FE2]">
+                Password Settings
+              </h4>
             </Link>
-            <h4 className="font-[400] text-sm text-inner text-[#737373] ext-center p-2 border-b border-b-[#FE0FE2]">
-            Notification Settings
+            <h4 className="font-[400] text-sm text-inner text-[#737373] text-center p-2 border-b border-b-[#FE0FE2]">
+              Notification Settings
             </h4>
-            
-            </div>
+          </div>
           <div className="my-5 p-2">
             <div className="my-2 flex justify-between">
-                
-                <div>
-                    <p className="text-sm font-inner font-[500] text-[#8083A3]">
-                    Notification type
-                    </p>
-                    <h4 className="text-md font-[700]">Progressive Notification</h4>
-                </div>
-                <Toggle
-                    pressed={support === false}
-                    onPressedChange={handleToggleChange}
-                    className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
-                    >
-                    <div
-                        className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
-                        support === false ? "translate-x-2" : "translate-x-[-6px]"
-                        }`}
-                    />
-                </Toggle>
-
+              <div>
+                <p className="text-sm font-inner font-[500] text-[#8083A3]">
+                  Notification type
+                </p>
+                <h4 className="text-md font-[700]">Progressive Notification</h4>
+              </div>
+              <Toggle
+                pressed={proNoti === false}
+                onPressedChange={handleProNoti}
+                className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
+              >
+                <div
+                  className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
+                    proNoti === false
+                      ? "translate-x-[-6px]"
+                      : "translate-x-[6px]"
+                  }`}
+                />
+              </Toggle>
             </div>
             <SelectSeparator className="my-4" />
             <div className="my-2 flex justify-between">
-                
-                <div>
-                    <p className="text-sm font-inner font-[500] text-[#8083A3]">
-                    Product updates
-                    </p>
-                    <h4 className="text-md font-[700]">Receive messages from our platform</h4>
-                </div>
-                <Toggle
-                    pressed={support == false}
-                    onPressedChange={handleToggleChange}
-                    className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
-                >
-                    <div
-                    className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
-                        support == false ? "translate-x-2" : "translate-x-[-6px]"
-                    }`}
-                    />
-                </Toggle>
-            </div>
-            <SelectSeparator className="my-4" />
-
-            <div className="my-2 flex justify-between">
-                
-                <div>
-                    <p className="text-sm font-inner font-[500] text-[#8083A3]">
-                    Notification
-                    </p>
-                    <h4 className="text-md font-[700]">Get push Notifications</h4>
-                </div>
-                <Toggle
-                    pressed={support == false}
-                    onPressedChange={handleToggleChange}
-                    className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
-                >
-                    <div
-                    className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
-                        support == false ? "translate-x-2" : "translate-x-[-6px]"
-                    }`}
-                    />
-                </Toggle>
+              <div>
+                <p className="text-sm font-inner font-[500] text-[#8083A3]">
+                  Product updates
+                </p>
+                <h4 className="text-md font-[700]">
+                  Receive messages from our platform
+                </h4>
+              </div>
+              <Toggle
+                pressed={receiveMsg == false}
+                onPressedChange={handleReceiveMsg}
+                className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
+              >
+                <div
+                  className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
+                    receiveMsg === false
+                      ? "translate-x-[-6px]"
+                      : "translate-x-[6px]"
+                  }`}
+                />
+              </Toggle>
             </div>
             <SelectSeparator className="my-4" />
 
             <div className="my-2 flex justify-between">
-                
-                <div>
-                    <p className="text-sm font-inner font-[500] text-[#8083A3]">
-                    Promotions and tips
-                    </p>
-                    <h4 className="text-md font-[700]">Receive coupons, promotions, surveys</h4>
-                </div>
-                <Toggle
-                    pressed={support == false}
-                    onPressedChange={handleToggleChange}
-                    className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
-                >
-                    <div
-                    className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
-                        support == false ? "translate-x-2" : "translate-x-[-6px]"
-                    }`}
-                    />
-                </Toggle>
+              <div>
+                <p className="text-sm font-inner font-[500] text-[#8083A3]">
+                  Notification
+                </p>
+                <h4 className="text-md font-[700]">Get push Notifications</h4>
+              </div>
+              <Toggle
+                pressed={getPush == false}
+                onPressedChange={handleGetPush}
+                className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
+              >
+                <div
+                  className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
+                    getPush === false
+                      ? "translate-x-[-6px]"
+                      : "translate-x-[6px]"
+                  }`}
+                />
+              </Toggle>
             </div>
             <SelectSeparator className="my-4" />
-           
+
             <div className="my-2 flex justify-between">
-                
-                <div>
-                    <p className="text-sm font-inner font-[500] text-[#8083A3]">
-                    Account support
-                    </p>
-                    <h4 className="text-md font-[700]">Receive messages about your account, legal alerts</h4>
-                </div>
-                <Toggle
-                    pressed={support == false}
-                    onPressedChange={handleToggleChange}
-                    className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
-                >
-                    <div
-                    className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
-                        support == false ? "translate-x-2" : "translate-x-[-6px]"
-                    }`}
-                    />
-                </Toggle>
+              <div>
+                <p className="text-sm font-inner font-[500] text-[#8083A3]">
+                  Promotions and tips
+                </p>
+                <h4 className="text-md font-[700]">
+                  Receive coupons, promotions, surveys
+                </h4>
+              </div>
+              <Toggle
+                pressed={receiveCupon == false}
+                onPressedChange={handleReceiveCupon}
+                className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
+              >
+                <div
+                  className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
+                    receiveCupon === false
+                      ? "translate-x-[-6px]"
+                      : "translate-x-[6px]"
+                  }`}
+                />
+              </Toggle>
             </div>
             <SelectSeparator className="my-4" />
 
-
-
+            <div className="my-2 flex justify-between">
+              <div>
+                <p className="text-sm font-inner font-[500] text-[#8083A3]">
+                  Account support
+                </p>
+                <h4 className="text-md font-[700]">
+                  Receive messages about your account, legal alerts
+                </h4>
+              </div>
+              <Toggle
+                pressed={receiveAccount == false}
+                onPressedChange={handleReceiveAccount}
+                className={`relative w-10 h-6 bg-[#00D066] dark:bg-[#00D066] rounded-full`}
+              >
+                <div
+                  className={`block w-4 h-4 bg-white dark:bg-gray-800 rounded-full shadow-md transform transition-transform ${
+                    receiveAccount === false
+                      ? "translate-x-[-6px]"
+                      : "translate-x-[6px]"
+                  }`}
+                />
+              </Toggle>
+            </div>
+            <SelectSeparator className="my-4" />
           </div>
-            
-        
         </Card>
-        
-        
       </div>
     </ScrollArea>
   );
--- src/app/(dashboard)/dashboard/settings/password/page.tsx
+++ src/app/(dashboard)/dashboard/settings/password/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 import { Breadcrumbs } from "@/components/breadcrumbs";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { FiCopy, FiEdit, FiLink, FiLink2, FiSend } from "react-icons/fi";
 import { Card } from "@/components/ui/card";
 import { Lexend } from "next/font/google";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Select, SelectContent, SelectItem, SelectSeparator, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { SelectSeparator } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
-import { Toggle } from "@/components/ui/toggle";
 import { useState } from "react";
 
 const breadcrumbItems = [
@@ -23,10 +19,10 @@ const lexend = Lexend({
   subsets: ["vietnamese"],
 });
 export default function Page() {
-    const [support, setSupport] = useState(false);
-    const handleToggleChange = () => {
-        setSupport(!support);
-      };
+  const [support, setSupport] = useState(false);
+  const handleToggleChange = () => {
+    setSupport(!support);
+  };
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4  p-4 pt-6 md:p-8 mb-16">
@@ -40,63 +36,82 @@ export default function Page() {
             <Breadcrumbs items={breadcrumbItems} />
           </div>
         </div>
-      
-        <Card className="w-full border-none px-5 rounded-xl p-5 mt-5 ">
-            <div className="grid grid-cols-3 w-1/2 my-4 ">
-            
-            <Link href="/dashboard/settings">
-            <h4 className="font-normal text-sm text-center p-2 text-[#737373] border-b border-b-[#D9DFEB] cursor-pointer hover:border-b-[#FE0FE2]">
-                General Settings
-            </h4>
 
+        <Card className="w-full border-none px-5 rounded-xl p-5 mt-5 ">
+          <div className="grid grid-cols-3 w-1/2 my-4 ">
+            <Link href="/dashboard/settings">
+              <h4 className="font-normal text-sm text-center p-2 text-[#737373] border-b border-b-[#D9DFEB] cursor-pointer hover:border-b-[#FE0FE2]">
+                General Settings
+              </h4>
             </Link>
-            
+
             <h4 className="font-[400] text-sm text-inner text-[#FE0FE2] text-center p-2 border-b border-b-[#FE0FE2] ">
-            Password Settings
+              Password Settings
             </h4>
             <Link href="/dashboard/settings/notification">
-            <h4 className="font-[400] text-sm text-inner text-[#737373] text-center p-2 border-b border-b-[#D9DFEB] cursor-pointer hover:border-b-[#FE0FE2]">
-            Notification Settings
-            </h4>
+              <h4 className="font-[400] text-sm text-inner text-[#737373] text-center p-2 border-b border-b-[#D9DFEB] cursor-pointer hover:border-b-[#FE0FE2]">
+                Notification Settings
+              </h4>
             </Link>
-            </div>
-          
-            <div className="space-y-1">
-                <h4 className="text-lg font-[600] text-[#101828] mt-5 dark:text-white">Password</h4>
-                <p className="text-sm font-inner font-[400] text-[14px] text-[#475467] my-2 dark:text[#D7D9DA]">
-                Please enter your current password to change your password.
-                </p>
-            </div>
-            <SelectSeparator className="my-4" />
-            <div className="flex gap-5 my-5">
-                <h4 className="text-sm w-[280px] font-inner font-[600] text-[#344054] dark:text-white">Current password</h4>
-                <Input type="password" placeholder="••••••••" className="w-[512px] "></Input>
-            </div>
+          </div>
 
-            <SelectSeparator className="my-4" />
-            <div className="flex gap-5 my-5">
-                <h4 className="text-sm w-[280px] font-inner font-[600] text-[#344054] dark:text-white">New password</h4>
-                <div>
-                <Input type="password" placeholder="••••••••" className="w-[512px] "></Input>
-                <p className="text-sm font-inner font-[400] text-[14px] text-[#475467] my-2 dark:text[#D7D9DA]">
+          <div className="space-y-1">
+            <h4 className="text-lg font-[600] text-[#101828] mt-5 dark:text-white">
+              Password
+            </h4>
+            <p className="text-sm font-inner font-[400] text-[14px] text-[#475467] my-2 dark:text[#D7D9DA]">
+              Please enter your current password to change your password.
+            </p>
+          </div>
+          <SelectSeparator className="my-4" />
+          <div className="flex gap-5 my-5">
+            <h4 className="text-sm w-[280px] font-inner font-[600] text-[#344054] dark:text-white">
+              Current password
+            </h4>
+            <Input
+              id="password"
+              type="password"
+              placeholder="••••••••"
+              className="w-[512px] "
+            ></Input>
+          </div>
+
+          <SelectSeparator className="my-4" />
+          <div className="flex gap-5 my-5">
+            <h4 className="text-sm w-[280px] font-inner font-[600] text-[#344054] dark:text-white">
+              New password
+            </h4>
+            <div>
+              <Input
+                id="password"
+                type="password"
+                placeholder="••••••••"
+                className="w-[512px] "
+              ></Input>
+              <p className="text-sm font-inner font-[400] text-[14px] text-[#475467] my-2 dark:text[#D7D9DA]">
                 Your new password must be more than 8 characters.
-                </p>
-                </div>
-               
+              </p>
             </div>
+          </div>
 
-            <SelectSeparator className="my-4" />
-            <div className="flex gap-5 my-5">
-                <h4 className="text-sm w-[280px] font-inner font-[600] text-[#344054] dark:text-white">Confirm new password</h4>
-                <Input type="password" placeholder="••••••••" className="w-[512px] "></Input>
-            </div>
-            <div className="flex gap-5 justify-end mb-5">
-                <Button className="font-inner font-[700] text-[16px] w-[110px] h-[50px] ">Save</Button>
-            </div>
+          <SelectSeparator className="my-4" />
+          <div className="flex gap-5 my-5">
+            <h4 className="text-sm w-[280px] font-inner font-[600] text-[#344054] dark:text-white">
+              Confirm new password
+            </h4>
+            <Input
+              id="password"
+              type="password"
+              placeholder="••••••••"
+              className="w-[512px] "
+            ></Input>
+          </div>
+          <div className="flex gap-5 justify-end mb-5">
+            <Button className="font-inner font-[700] text-[16px] w-[110px] h-[50px] ">
+              Save
+            </Button>
+          </div>
         </Card>
-        
-        
-        
       </div>
     </ScrollArea>
   );
